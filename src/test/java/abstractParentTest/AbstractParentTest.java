3dfecc1b1d481d7b7ef9cd456c9a1161c05e9fc0
Hobbit_D <Hobbit.love.vedmochka@gmail.com>
2019-12-29 11:46:23 +0200

    2

--- src/test/java/abstractParentTest/AbstractParentTest.java
+++ src/test/java/abstractParentTest/AbstractParentTest.java
@@ -1,9 +1,19 @@
 package abstractParentTest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+import libs.ConfigProperties;
+import org.aeonbits.owner.ConfigFactory;
+import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -27,6 +37,10 @@ public class AbstractParentTest {
     protected LoginPage loginPage;
     protected RestorePage restorePage;
     protected FirstDepositPage firstDepositPage;
+    protected static ConfigProperties configProperties =
+            ConfigFactory.create(ConfigProperties.class);
+    protected Logger logger = Logger.getLogger(getClass());
+
     @Before
     public void SetUp() throws Exception {
         webDriver = driverInit();
@@ -46,28 +60,60 @@ public class AbstractParentTest {
 
     private WebDriver driverInit() throws Exception {
         String browser = System.getProperty("browser");
-        if ((browser == null) || ("chrome".equalsIgnoreCase(browser))){
+        if ((browser == null) || ("chrome".equalsIgnoreCase(browser))) {
             WebDriverManager.chromedriver().setup();
             return new ChromeDriver();
-        } else if("firefox".equalsIgnoreCase(browser)){
+        } else if ("firefox".equalsIgnoreCase(browser)) {
             WebDriverManager.firefoxdriver().setup();
             return new FirefoxDriver();
-        } else if("ie".equalsIgnoreCase(browser)){
+        } else if ("ie".equalsIgnoreCase(browser)) {
             WebDriverManager.iedriver().arch32().setup();
             return new InternetExplorerDriver();
-        } else if("opera".equalsIgnoreCase(browser)){
+        } else if ("opera".equalsIgnoreCase(browser)) {
             WebDriverManager.operadriver().setup();
             return new OperaDriver();
         } else {
             throw new Exception("Check browser var ");
         }
     }
-//    @After
-//    public void tearDown(){
-//        webDriver.quit();
-//    }
 
-    public static void checkExpectedResult(String message, boolean actualResult){
+    @After
+    public void tearDown() {
+    }
+
+    @Rule
+    public TestWatcher watchman = new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, Description description) {
+            screenshot();
+        }
+
+        @Attachment(value = "Page screenshot", type = "image/png")
+        public byte[] saveScreenshot(byte[] screenShot) {
+            return screenShot;
+        }
+
+        public void screenshot() {
+            if (webDriver == null) {
+                logger.info("Driver for screenshot not found");
+                return;
+            }
+            saveScreenshot(((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES));
+        }
+
+        @Override
+        protected void finished(Description description) {
+            logger.info(String.format("Finished test: %s::%s", description.getClassName(), description.getMethodName()));
+            try {
+                webDriver.quit();
+            } catch (Exception e) {
+                logger.error(e);
+            }
+        }
+    };
+
+    @Step
+    protected void checkExpectedResult(String message, boolean actualResult) {
         Assert.assertEquals(message, true, actualResult);
     }
 }
